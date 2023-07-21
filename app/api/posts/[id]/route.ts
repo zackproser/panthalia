c1b884445192e550ab4a6532571654a5c9cfb6a9
Zachary Proser <zackproser@gmail.com>
2023-07-21 11:45:46 -0400

    Implement basic post routing

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -1,19 +1,37 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+
+  console.dir(params)
+
+  const id = params.id
 
   try {
 
-    // Get all posts 
-    const result = await sql`
+    if (id) {
+      // Get single post if id provided  
+      const result = await sql`
+        SELECT * 
+        FROM posts
+        WHERE id = ${id}
+      `;
+
+      return new Response(JSON.stringify(result.rows[0]), {
+        status: 200
+      });
+
+    } else {
+      // Get all posts if no id
+      const result = await sql`
         SELECT * 
         FROM posts
       `;
 
-    return new Response(JSON.stringify({ posts: result.rows }), {
-      status: 200
-    });
+      return new Response(JSON.stringify({ posts: result.rows }), {
+        status: 200
+      });
+    }
   } catch (error) {
 
     console.log(`error: ${error}`);
