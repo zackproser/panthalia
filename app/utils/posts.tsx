626e9356186eb0389ded11f350e084d01d6ecfbd
Zachary Proser <zackproser@gmail.com>
2023-08-26 22:13:23 -0400

    Add missing curly brace

--- app/utils/posts.tsx
+++ app/utils/posts.tsx
@@ -57,11 +57,12 @@ ${baseMdx}
 ${content}
 `
   return mdx
+}
 
-  export function truncate(text: string, length: number) {
-    if (text.length <= length) {
-      return text;
-    }
-    return text.substring(0, length) + "...";
+export function truncate(text: string, length: number) {
+  if (text.length <= length) {
+    return text;
   }
+  return text.substring(0, length) + "...";
+}
 
