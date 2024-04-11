b1d6659e5f1b53b97a91232a147f2e0bb70d4542
Zachary Proser <zackproser@gmail.com>
2024-04-11 09:43:27 -0400

    Fix build error and display last saved time

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -244,8 +244,7 @@ function EditPost({ post }) {
     return () => clearInterval(autosaveInterval); // Clean up on component unmount
   }, [title, summary, content, imagePrompts]);
 
-  // Add this line where you want to display the last saved timestamp in the UI
-  // <p>Last saved {humanReadableTimeSinceLastSaved()}</p>
+
   return (
     <>
       {showModal && (
@@ -259,7 +258,7 @@ function EditPost({ post }) {
       )}
 
 
-      <form onSubmit={handleSubmit} className="space-y-12">
+      <form className="space-y-12">
         <div className="border-b border-grey/10 pb-12">
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
@@ -276,6 +275,7 @@ function EditPost({ post }) {
                   className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
                 />
               </div>
+              <p className="text-white">Last saved {humanReadableTimeSinceLastSaved()}</p>
             </div>
 
             <div className="col-span-full">
